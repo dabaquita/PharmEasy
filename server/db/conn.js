d66d1913e3a96a3e3fc9a80522695be12e256c6a
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-11-13 13:45:39 -0500

    Fixed the database and server - now working

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,25 +1,28 @@
-const { MongoClient } = require("mongodb");
+import * as mongodb from 'mongodb';
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" });
 const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
+if (Db === undefined) { throw new Error("My ATLAS_URI is undefined");}
+
+const client = new mongodb.MongoClient(Db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
 var _db;
 
-module.exports = {
-    connectToServer: function (callback) {
-        client.connect(function (err, db) {
-            // Verify we got a good "db" object
-            if (db) {
-                _db = db.db("myFirstDatabase");
-                console.log("Successfully connected to MongoDB.");
-            }
-            return callback(err);
-        });
-    },
+export function connectToServer(callback) {
+    client.connect(function (err, db) {
+        // Verify we got a good "db" object
+        if (db) {
+            _db = db.db("myFirstDatabase");
+            console.log("Successfully connected to MongoDB.");
+        }
+        return callback(err);
+    });
+};
 
-    getDb: function () {
-        return _db;
-    },
+export function getDb() {
+    return _db;
 };
