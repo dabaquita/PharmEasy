bfc4d1b28122be26776307fcc734e4079762dac9
Jon Abrigo <jonabrigo@knights.ucf.edu>
2021-11-13 18:44:19 -0500

    adding medicalInfo/billingInfo controllers

---
+++ server/controllers/medicalInfo.js
@@ -0,0 +1,19 @@
+import medicalInfoModal from "../models/medicalInfo";
+
+export const addMedicalInfo = async (req, res) => {
+    const { drugName, ndc, quantity, remaining, refills, prescriber, usedFor } = req.body;
+
+    try {
+        const duplicateMedicalinfo = await medicalInfoModal.findOne({ ndc });
+
+        if (duplicateMedicalinfo) return res.status(400).json({ message: "Medical info already exists" });
+
+        const result = await medicalInfoModal.create({ drugName, ndc, quantity, remaining, refills, prescriber, usedFor });
+
+        res.status(201).json({ result });
+
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+        console.log(error);
+    }
+};
