a62fc03a32adedd323b0731f62ddd6b65739b1e4
Jon Abrigo <jonabrigo@knights.ucf.edu>
2021-11-13 11:08:32 -0500

    adding models

--- server/server.js
+++ server/server.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:3000/PharmEasyDB')
+
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 3000;
-app.use(cors());
-app.use(express.json());
 
 // get driver connection
 const dbo = require("./db/conn");
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
+
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+// Register
+app.post('/api/register', (req, res) => {
+  console.log(req.body)
+  res.json({ status: 'ok' })
+
+
+})
