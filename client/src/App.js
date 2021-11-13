74f597fe9775683d664640a6fd8943a6443a96f2
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-11-13 17:34:43 -0500

    Fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Container } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import LoginPage from './pages/LoginPage';
@@ -15,68 +14,6 @@ const theme = createTheme({
 })
 
 function App() {
-=======
-import { Grid, Container, Box } from '@mui/material';
-import './App.css';
-import FirstNameTextField from './components/FirstNameTextField/FirstNameTextField';
-import LastNameTextField from './components/LastNameTextField/LastNameTextField';
-import EmailTextField from './components/EmailTextField/EmailTextField';
-import PasswordTextField from './components/PasswordTextField/PasswordTextField';
-import SignInButton from './components/SignInButton/SignInButton';
-import SignUpButton from './components/SignUpButton/SignUpButton';
-import ForgotPassword from './components/ForgotPasswordHyperlink/ForgotPasswordHyperlink';
-import BasicInfoCard from './components/BasicInfoCard/BasicInfoCard';
-import InsuranceInfoCard from './components/InsuranceInfoCard/InsuranceInfoCard';
-import MedicationListGrid from './components/MedicationList/MedicationListGrid';
-
-
-function App() {
-  return (
-    <Container component="main" maxWidth="xs">
-          <Box 
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems:'center',
-            }}
-          >
-            <Grid container spacing={2}>
-
-              <Grid>
-                <FirstNameTextField/>
-              </Grid>
-              <Grid>
-                <LastNameTextField/>
-              </Grid>
-
-              <Grid>
-                <EmailTextField/>
-                <PasswordTextField/>
-                <ForgotPassword/>
-              </Grid>
-
-              <Grid>
-                <SignInButton/>
-              </Grid>
-              <Grid>
-                <SignUpButton/> 
-                <BasicInfoCard/> <br />
-                <InsuranceInfoCard/> <br />
-
-                <MedicationListGrid />
-
-              </Grid>
-
-
-
-
-            </Grid>
-
-          </Box>
-
-    </Container>
->>>>>>> master
 
   return (
     <ThemeProvider theme={theme}>
