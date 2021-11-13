eaf88c64eacf82b20095378fc5778db4495a889a
Jon Abrigo <jonabrigo@knights.ucf.edu>
2021-11-12 23:10:25 -0500

    slight changes

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ app.use(express.json());
 
 // get driver connection
 const dbo = require("./db/conn");
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
+
   });
   console.log(`Server is running on port: ${port}`);
 });
