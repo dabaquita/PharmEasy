e3c2a2b856539ece29e11b28691ced0245a3bf5b
Resilliance <JKEsguerra2050@yahoo.com>
2021-11-13 11:50:38 -0500

    Created BasicInforCard for patient info

---
+++ client/src/components/BasicInfoCard/BasicInfoCard.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+const BasicInfoCard = (
+  <React.Fragment>
+    <CardContent>
+      <Typography variant="h5" component="div">
+        Esguerra, Justin Kieran
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        05/02/1997   -   954-682-****
+      </Typography>
+      <Typography variant="body2">
+        Address: 13007 Marsh St. Orlando 32929
+        <br /> 
+        <br />
+      </Typography>
+      <Typography sx={{ mb: 0.5 }} variant="body3">
+        Hypertension | No Allergy
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Edit Profile</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+export default function OutlinedCard() {
+  return (
+    <Box sx={{ minWidth: 400 }}>
+      <Card variant="outlined">{BasicInfoCard}</Card>
+    </Box>
+  );
+}
