48604274d7924c8c811c5780908ebce23b35ee83
Jon Abrigo <jonabrigo@knights.ucf.edu>
2021-11-13 14:44:18 -0500

    adding models

---
+++ server/models/patient.js
@@ -0,0 +1,19 @@
+
+const Patient = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        dob: { type: Date, required: true },
+        phone: { type: String },
+        address: { type: String },
+        address: { type: String },
+        allergy: { type: String },
+        medicalConditions: { type: String },
+        medications: { type: String },
+        medList: { type: String }
+    },
+    { collection: 'patients' }
+)
+
+const model = mongoose.model('PatientData', Patient)
+
+export default model;
