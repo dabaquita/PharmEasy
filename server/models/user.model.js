2c75253ddbf8a85300f549ebf2405ebf6c8014e1
Jon Abrigo <jonabrigo@knights.ucf.edu>
2021-11-13 11:22:24 -0500

    Update user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const User = new mongoose.Schema(
     {
         name: { type: String, required: true },
-        email: { type: String, reuqired: true },
+        email: { type: String, reuqired: true, unique: true },
         password: { type: String, required: true },
         quote: { type: String }
     },
