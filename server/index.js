0cb074ebedfebf6d1e2c856e07f978723d62798e
Jon Abrigo <jonabrigo@knights.ucf.edu>
2021-11-13 20:25:08 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRouter from "./routes/user.js";
+import patientRouter from "./routes/patient.js";
+import medicalInfoRouter from "./routes/medicalInfo.js";
+import billingInfoRouter from "./routes/billingInfo.js";
 
 const app = express();
 
@@ -12,6 +15,9 @@ app.use(express.urlencoded())
 app.use(cors());
 
 app.use("/user", userRouter);
+app.use("/patient", patientRouter);
+app.use("/medicalInfo", medicalInfoRouter);
+app.use("/billingInfo", billingInfoRouter);
 
 dotenv.config({ path: "./config.env" });
 const CONNECTION_URI = process.env.ATLAS_URI;
@@ -19,10 +25,9 @@ const PORT = process.env.port || 3000;
 
 mongoose
   .connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log( 'Database Successfully Connected' ))
+  .then(() => console.log('Database Successfully Connected'))
   .catch(err => console.log(`Database connection failed due to ${err}`));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-  
